161280e09020bc87a638f02483c413a568b30636
bit1100 <surajkeshari1100@gmail.com>
2021-10-21 18:34:50 +0530

    Proper Linking of Page is done with Javascript

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 const siteContentWrapper = document.querySelector(".site-content-wrapper");
 const navTriggerImg = document.querySelector(".nav-trigger__img");
+const navbarLinks = document.querySelectorAll(".nav-bg .second a");
 
 navTriggerImg.addEventListener("click", () => {
-    if (siteContentWrapper.classList.contains("scaled")) {
-        siteContentWrapper.classList.remove("scaled");
-        navTriggerImg.style.transform = "rotate(0deg)";
-    } else {
-        navTriggerImg.style.transform = "rotate(540deg)";
-        siteContentWrapper.classList.add("scaled");
-    }
+  if (siteContentWrapper.classList.contains("scaled")) {
+    siteContentWrapper.classList.remove("scaled");
+    navTriggerImg.style.transform = "rotate(0deg)";
+  } else {
+    navTriggerImg.style.transform = "rotate(540deg)";
+    siteContentWrapper.classList.add("scaled");
+  }
+});
+
+navbarLinks.forEach((link) => {
+  link.onclick = () => {
+    siteContentWrapper.classList.remove("scaled");
+    navTriggerImg.style.transform = "rotate(0deg)";
+  };
 });
